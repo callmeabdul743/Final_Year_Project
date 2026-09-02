9b67354fcef2fa6aef33c55570d08cb1b9c8ea1a
callmeabdul743 <abdullahkhalidmul@gmail.com>
2026-09-02 11:29:52 +0500

    Day4, Make a login page function

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+
+app.secret_key = "fyp-secret-key"
 
 
 @app.route("/")
@@ -8,5 +10,40 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "admin" and password == "1234":
+            session["user"] = username
+            return redirect(url_for("dashboard"))
+
+        else:
+            return "Invalid username or password"
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("dashboard.html")
+
+
+@app.route("/logout")
+def logout():
+
+    session.pop("user", None)
+
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
-    app.run(debug=True,port=8080)
+    app.run(debug=True)
